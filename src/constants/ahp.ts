cf2b3b1c8a92e9f81a661f022b06240ccc7a980f
Rifqi <rifqirrd8@gmail.com>
2025-05-28 07:43:46 +0700

    fix: making criterion draggable

--- src/constants/ahp.ts
+++ src/constants/ahp.ts
@@ -1,29 +1,29 @@
 import { ref } from 'vue';
 
-export const criteriaPairs= [
-  ["Akreditasi", "Biaya"],
-  ["Akreditasi", "Persentase Kelulusan"],
-  ["Akreditasi", "Jarak"],
-  ["Akreditasi", "Jumlah Jurusan"],
-  ["Biaya", "Persentase Kelulusan"],
-  ["Biaya", "Jarak"],
-  ["Biaya", "Jumlah Jurusan"],
-  ["Persentase Kelulusan", "Jarak"],
-  ["Persentase Kelulusan", "Jumlah Jurusan"],
-  ["Jarak", "Jumlah Jurusan"],
-];
+// export const criteriaPairs= [
+//   ["Akreditasi", "Biaya"],
+//   ["Akreditasi", "Persentase Kelulusan"],
+//   ["Akreditasi", "Jarak"],
+//   ["Akreditasi", "Jumlah Jurusan"],
+//   ["Biaya", "Persentase Kelulusan"],
+//   ["Biaya", "Jarak"],
+//   ["Biaya", "Jumlah Jurusan"],
+//   ["Persentase Kelulusan", "Jarak"],
+//   ["Persentase Kelulusan", "Jumlah Jurusan"],
+//   ["Jarak", "Jumlah Jurusan"],
+// ];
 
-export const ahpScale = [
-  { value: 9, label: "Ekstrem lebih penting" },
-  { value: 7, label: "Sangat lebih penting" },
-  { value: 5, label: "Lebih penting" },
-  { value: 3, label: "Sedikit lebih penting" },
-  { value: 1, label: "Sama penting" },
-  { value: 1 / 3, label: "Sedikit kurang penting" },
-  { value: 1 / 5, label: "Kurang penting" },
-  { value: 1 / 7, label: "Sangat kurang penting" },
-  { value: 1 / 9, label: "Ekstrem kurang penting" },
-];
+// export const ahpScale = [
+//   { value: 9, label: "Ekstrem lebih penting" },
+//   { value: 7, label: "Sangat lebih penting" },
+//   { value: 5, label: "Lebih penting" },
+//   { value: 3, label: "Sedikit lebih penting" },
+//   { value: 1, label: "Sama penting" },
+//   { value: 1 / 3, label: "Sedikit kurang penting" },
+//   { value: 1 / 5, label: "Kurang penting" },
+//   { value: 1 / 7, label: "Sangat kurang penting" },
+//   { value: 1 / 9, label: "Ekstrem kurang penting" },
+// ];
 
 export const ahpStats = ref<{
   consistencyRatio: number,
@@ -32,26 +32,39 @@ export const ahpStats = ref<{
   // matrixSize: number
 } | null>(null);
 
-export function ahpLabel(value: number): string {
-  switch (value) {
-    case 1: return 'Mutlak kurang penting';
-    case 2: return 'Sangat kurang penting';
-    case 3: return 'Kurang penting';
-    case 4: return 'Sedikit kurang penting';
-    case 5: return 'Sama penting';
-    case 6: return 'Sedikit lebih penting';
-    case 7: return 'Lebih penting';
-    case 8: return 'Sangat lebih penting';
-    case 9: return 'Mutlak lebih penting';
-    default: return `Skor: ${value}`;
-  }
+// export function ahpLabel(value: number): string {
+//   switch (value) {
+//     case 1: return 'Mutlak kurang penting';
+//     case 2: return 'Sangat kurang penting';
+//     case 3: return 'Kurang penting';
+//     case 4: return 'Sedikit kurang penting';
+//     case 5: return 'Sama penting';
+//     case 6: return 'Sedikit lebih penting';
+//     case 7: return 'Lebih penting';
+//     case 8: return 'Sangat lebih penting';
+//     case 9: return 'Mutlak lebih penting';
+//     default: return `Skor: ${value}`;
+//   }
+// }
+
+// export const getColorClass = (kriteria:string) => {
+//   if (kriteria === "Akreditasi") return "text-red-600"
+//   if (kriteria === "Biaya") return "text-green-600"
+//   if (kriteria === "Jarak") return "text-yellow-600"
+//   if (kriteria === "Persentase Kelulusan") return "text-purple-600"
+//   if (kriteria === "Jumlah Jurusan") return "text-pink-600"
+//   return "text-gray-600" // default kalau ga cocok
+// }
+
+export interface Criterion {
+  id: number;
+  label: string;
 }
 
-export const getColorClass = (kriteria:string) => {
-  if (kriteria === "Akreditasi") return "text-red-600"
-  if (kriteria === "Biaya") return "text-green-600"
-  if (kriteria === "Jarak") return "text-yellow-600"
-  if (kriteria === "Persentase Kelulusan") return "text-purple-600"
-  if (kriteria === "Jumlah Jurusan") return "text-pink-600"
-  return "text-gray-600" // default kalau ga cocok
-}
+export const criteria: Criterion[] = [
+  { id: 1, label: 'Akreditasi' },
+  { id: 2, label: 'Biaya' },
+  { id: 3, label: 'Jarak' },
+  { id: 4, label: 'Jumlah Jurusan' },
+  { id: 5, label: 'Persentase Kelulusan' },
+]
--- src/views/RecommendationPage.vue
+++ src/views/RecommendationPage.vue
@@ -3,10 +3,16 @@ import { ref, onMounted, computed, nextTick } from 'vue';
 import api from '@/utils/api';
 import AlertBox from '@/components/AlertBox.vue';
 import L from 'leaflet';
-import { criteriaPairs, ahpLabel, ahpStats, getColorClass } from '@/constants/ahp';
+import { criteria, ahpStats, type Criterion} from '@/constants/ahp';
 import '@/assets/animations.css';
+import draggable from 'vuedraggable';
 
-const criteriaWeights= ref<number[]>(Array(criteriaPairs.length).fill(5));
+
+
+const ranked= ref<Criterion[]>([...criteria]);
+const result= ref<[string, string, number][]>([]);
+
+// const criteriaWeights= ref<number[]>(Array(criteriaPairs.length).fill(5));
 const userLat= ref<number | null>(null);
 const userLong= ref<number | null>(null);
 const map= ref()
@@ -105,9 +111,12 @@ const canSubmit = computed(() => {
 
 const submitAHP = async() => {
   
-  const formattedWeights= criteriaPairs.map(([a, b], i) => [a, b, mapSliderToAHP(criteriaWeights.value[i])]);
+  const labels= ranked.value.map((c) => c.label)
+  result.value= generatePairwiseAHPFromRankedList(labels);
+  // const formattedWeights= criteriaPairs.map(([a, b], i) => [a, b, mapSliderToAHP(criteriaWeights.value[i])]);
+  
   const payload= {
-    criteriaWeights: formattedWeights,
+    criteriaWeights: result.value,
     userLat: userLat.value,
     userLong: userLong.value,
   };
@@ -148,6 +157,8 @@ const submitAHP = async() => {
       };
     }).sort((a, b) => b.skor - a.skor);
 
+    formError.value= false;
+
   } catch(err) {
     console.error('Error submitting AHP:', err);
     formError.value= true;
@@ -157,25 +168,22 @@ const submitAHP = async() => {
   }
 }
 
-function mapSliderToAHP(value: number): number {
-  switch (value) {
-    case 1: return 1 / 9;
-    case 2: return 1 / 7;
-    case 3: return 1 / 5;
-    case 4: return 1 / 3;
-    case 5: return 1;
-    case 6: return 3;
-    case 7: return 5;
-    case 8: return 7;
-    case 9: return 9;
-    default: return 1;
+function generatePairwiseAHPFromRankedList(ranked: string[]): [string, string, number][] {
+  const n= ranked.length;
+  const result: [string, string, number][] = [];
+
+  for (let i=0; i < n; i++) {
+    for (let j=i + 1; j < n; j++) {
+      const a= ranked[i]
+      const b= ranked[j];
+      const diff= j-i
+      const weight= Math.min(9, diff * 2 + 1)
+      result.push([a, b, weight]);
+    }
   }
+  return result;
 }
-
-
-
 </script>
-
 
 <template>
   <div class="p-6 max-w-2xl mx-auto">
@@ -205,60 +213,33 @@ function mapSliderToAHP(value: number): number {
       </button>
     </div>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
-    <div
-      v-for="(pair, index) in criteriaPairs"
-      :key="index"
-      class="space-y-2"
-    >
-      <label class="block font-semibold mb-2">
-         Seberapa penting 
-         <span :class="getColorClass(pair[0])">{{ pair[0] }}</span> 
-         dibanding 
-         <span :class="getColorClass(pair[1])">{{  pair[1] }}</span>?
-      </label>
-      <div class="flex flex-col gap-2">
+    <div class="max-w-xl py-4">
+      <h2 class="text-lg font-semibold mb-4">Urutkan Kriteria (Paling Penting ke Tidak Penting)</h2>
 
-      <div class="flex items-center justify-between gap-3 text-sm text-gray-500">
-        <span>Tidak penting</span>
-        <span>Sama penting</span>
-        <span>Lebih penting</span>
-      </div>
-
-      <input
-        type="range"
-        min="1"
-        max="9"
-        step="1"
-        v-model.number="criteriaWeights[index]"
-        class="w-full h-3 bg-gradient-to-r from-red-500 via-yellow-400 to-green-500 rounded-full appearance-none
-         focus:outline-none focus:ring-2 focus:ring-blue-500
-         [&::-webkit-slider-thumb]:appearance-none
-         [&::-webkit-slider-thumb]:h-5
-         [&::-webkit-slider-thumb]:w-5
-         [&::-webkit-slider-thumb]:rounded-full
-         [&::-webkit-slider-thumb]:bg-blue-600
-         [&::-webkit-slider-thumb]:border-2
-         [&::-webkit-slider-thumb]:border-white
-         [&::-webkit-slider-thumb]:shadow
-         [&::-webkit-slider-thumb]:transition-all
-         [&::-webkit-slider-thumb]:duration-200
-         [&::-webkit-slider-thumb]:hover:scale-110
-         [&::-moz-range-thumb]:appearance-none
-         [&::-moz-range-thumb]:h-5
-         [&::-moz-range-thumb]:w-5
-         [&::-moz-range-thumb]:rounded-full
-         [&::-moz-range-thumb]:bg-blue-600
-         [&::-moz-range-thumb]:border-2
-         [&::-moz-range-thumb]:border-white
-         [&::-moz-range-thumb]:shadow"
-      />
-
-      <div class="text-center text-blue-600 font-semibold">
-        {{ ahpLabel(criteriaWeights[index]) }}
-      </div>
-    </div>
-    </div>
+      <draggable
+        v-model="ranked"
+        item-key="id"
+        class="space-y-2"
+        ghost-class="ghost"
+        chosen-class="chosen"
+        drag-class="drag"
+        animation="200"
+      >
+        <template #item="{ element }">
+        <div
+          class="bg-white shadow-md rounded-lg px-4 py-3 mb-2 flex items-center justify-between hover:scale-[1.01] cursor-move border border-gray-200"
+        >
+          <span class="font-medium text-gray-800">
+            {{ element.label }}
+          </span>
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M4 8h16M4 16h16" />
+          </svg>
+        </div>
+      </template>
+      </draggable>
     </div>
 
     <button
@@ -309,3 +290,18 @@ function mapSliderToAHP(value: number): number {
     </div>
   </div>
 </template>
+
+<style scoped>
+.ghost {
+  opacity: 0.5;
+}
+
+.chosen {
+  transform: scale(1.05);
+  transition: transform 0.3s;
+}
+
+.drag {
+  transition: transform 0.2s ease;
+}
+</style>
